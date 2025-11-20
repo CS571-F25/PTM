2d35c782ced78bce2d78a33c1ae31c6c26dd12af
Kurinji <kurinjikumar2005@gmail.com>
2025-11-19 20:17:01 -0600

    some edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { HashRouter, Routes, Route } from 'react-router'
 import './App.css'
 import Home from './components/Home'
 import AboutMe from './components/AboutMe'
+import Settings from './components/Setting'
 
 function App() {
   return <HashRouter>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/about' element={<AboutMe/>}></Route>
+      <Route path='/settings' element={<Settings/>}></Route>
     </Routes>
   </HashRouter>
 }
